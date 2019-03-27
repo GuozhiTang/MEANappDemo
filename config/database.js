2d15c2803467bc54b59ebacd3eb3d5f4a13313b7
Guozhi Tang <tangguozhi53@gmail.com>
2019-03-27 10:39:46 -0400

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauthapi',
+  // database: 'mongodb://localhost:27017/meanauthapi',
+  database: 'mongodb+srv://guozhi:12345@meanappdemo-4p5ba.mongodb.net/test?retryWrites=true',
   secret: 'yoursecret'
 }
